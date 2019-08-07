c691891aed6d93b5a9b6782e4afab9dd6e02eebc
andziaania <andziaania@gmail.com>
2019-08-07 14:54:11 +0200

    nebular auth - making it work (removing InMemoryDataService)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { LoginExistsValidatorDirective } from './shared/login-exists-validator.directive';
-import { HttpClientModule} from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-import { NbPasswordAuthStrategy, NbAuthModule } from '@nebular/auth';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
-
-
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HttpClientModule } from '@angular/common/http';
+import { NbPasswordAuthStrategy, NbAuthModule } from '@nebular/auth';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegistrationComponent,
-    LoginExistsValidatorDirective,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, {dataEncapsulation: false}
-    ),
-    NbThemeModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
+    NbEvaIconsModule,
+    HttpClientModule,
     NbAuthModule.forRoot({
       strategies: [
         NbPasswordAuthStrategy.setup({
@@ -40,7 +29,6 @@ import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
       ],
       forms: {},
     }),
-    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
