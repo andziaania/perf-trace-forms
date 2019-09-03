d63b3fc6fa7176af6a26cc43279a4b7d31b5a8eb
andziaania <andziaania@gmail.com>
2019-09-03 15:57:04 +0200

    Users.component cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { NbThemeModule, NbDatepickerModule, NbButtonModule, NbSidebarModule, NbMenuModule } from '@nebular/theme';
+import { NbThemeModule, NbDatepickerModule, NbSidebarModule, NbMenuModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { HttpClientModule  } from '@angular/common/http';
 import { NbDummyAuthStrategy, NbAuthModule } from '@nebular/auth';
 
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MockBackendModule } from './mock-backend/mock-backend.module';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -23,8 +23,6 @@ import { AppComponent } from './app.component';
     HttpClientModule,
 
     NbThemeModule.forRoot({ name: 'default' }),
-    // NbLayoutModule.forRoot(),   // import in the feature module is also needed
-    // NbLayoutModule,
     NbMenuModule.forRoot(),
     NbSidebarModule.forRoot(),
     NbEvaIconsModule,
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,10 +1,4 @@
-import { ChartDataSets } from 'chart.js';
-import { UsersService } from './../../data-services/users.service';
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
-import { Observable, zip } from 'rxjs';
-
-import { ChartUsersComponent } from './chart-users/chart-users.component';
-import { map } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 import { TIME_RANGE } from './time-range';
 
@@ -14,7 +8,7 @@ import { TIME_RANGE } from './time-range';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements AfterViewInit {
+export class UsersComponent {
 
   timeRanges = [TIME_RANGE.DAY, TIME_RANGE.WEEK, TIME_RANGE.MONTH];
 
@@ -22,69 +16,11 @@ export class UsersComponent implements AfterViewInit {
 
   selectedDate = new Date();
 
-  constructor(private users: UsersService) {
-  }
-
-  ngAfterViewInit() {
-    // this.refreshTotalData();
-    // this.refreshNewReturningData();
-  }
-
-  /**
-   * Calling asynchronously data services of users activity in the current and the previous period
-   */
-  // private refreshNewReturningData() {
-  //   zip(
-  //     this.selectedTimeRange.getUsersActivityMethod$.call(this.users, this.selectedDate),
-  //     this.selectedTimeRange.getUsersActivityMethod$.call(this.users, this.selectedTimeRange.getPreviousMoment())
-  //   ).pipe(
-  //     map(([datasetCurr, datasetPrev]: [number[], number[]]) => [
-  //       { data: datasetCurr, label: `By ${this.selectedDate.toDateString()}`},
-  //       { data: datasetPrev, label: 'Previous'}
-  //     ])
-  //   ).subscribe((newData) => this.usersChart.setChartData(newData));
-  // }
-
-
-
-  // events
-  // handleSelectedTimeRangeChange(timeRange: string) {
-  //   this.selectedTimeRange = timeRange;
-    //event emitter
-
-
-    // for (const timeRangeElement of this.timeRanges) {
-    //   if (timeRangeElement.type === timeRange) {
-    //     this.selectedTimeRange = timeRangeElement;
-    //     break;
-    //   }
-    // }
-    // this.refreshTotalData();
-  // }
-
-  handleDateChange(newDate: Date) {
-    //event emitter
-    this.selectedDate = newDate;
-    // this.refreshTotalData();
+  constructor() {
   }
 
 
-  // public pushOne() {
-  //   this.lineChartData.forEach((x, i) => {
-  //     const num = this.generateNumber(i);
-  //     const data: number[] = x.data as number[];
-  //     data.push(num);
-  //   });
-  //   this.lineChartLabels.push(`Label ${this.lineChartLabels.length}`);
   // }
 
-  // public changeColor() {
-  //   this.lineChartColors[2].borderColor = 'green';
-  //   this.lineChartColors[2].backgroundColor = `rgba(0, 255, 0, 0.3)`;
-  // }
 
-  // public changeLabel() {
-  //   this.lineChartLabels[2] = ['1st Line', '2nd Line'];
-  //   // this.chart.update();
-  // }
 }
