f3df3f627dccb0aedc2c47b45d12d6780ff5932b
andziaania <andziaania@gmail.com>
2019-09-05 13:15:31 +0200

    For production build - removing unused components

--- src/app/deprecated/shared/login-exists-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LoginExistsValidatorDirective } from './login-exists-validator.directive';
-
-describe('LoginExistsValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LoginExistsValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
