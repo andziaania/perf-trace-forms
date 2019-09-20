8846a8f70e39ecdd4f4c73fcd91a708b1e0450a8
andziaania <andziaania@gmail.com>
2019-09-20 14:32:42 +0200

    users.service: changeng timestamp parameter to ISO date

--- src/app/data-services/users.service.ts
+++ src/app/data-services/users.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 const URL = 'api/users';
 const UTYPE_TOTAL = 'total';
@@ -18,53 +18,53 @@ export class UsersService {
 
   constructor(private http: HttpClient) { }
 
-  getActiveUsersCount(): Observable<number> {
-    return of(324);
-  }
+  // getActiveUsersCount(): Observable<number> {
+  //   return of(324);
+  // }
 
   getDailyUsersActivity(date: Date): Observable<number[]> {
-    const reqUrl = `${URL}/${UTYPE_TOTAL}/${RANGE_DAY}/${date.getTime()}`;
+    const reqUrl = `${URL}/${UTYPE_TOTAL}/${RANGE_DAY}?date=${date.toISOString()}`;
     return this.http.get<number[]>(reqUrl);
   }
 
   getWeeklyUsersActivity(date: Date): Observable<number[]> {
-    const reqUrl = `${URL}/${UTYPE_TOTAL}/${RANGE_WEEK}/${date.getTime()}`;
+    const reqUrl = `${URL}/${UTYPE_TOTAL}/${RANGE_WEEK}?date=${date.toISOString()}`;
     return this.http.get<number[]>(reqUrl);
   }
 
   getMonthlyUsersActivity(date: Date): Observable<number[]> {
-    const reqUrl = `${URL}/${UTYPE_TOTAL}/${RANGE_MONTH}/${date.getTime()}`;
+    const reqUrl = `${URL}/${UTYPE_TOTAL}/${RANGE_MONTH}?date=${date.toISOString()}`;
     return this.http.get<number[]>(reqUrl);
   }
 
 
   getDailyNewUsersActivity(date: Date):  Observable<number[]> {
-    const reqUrl = `${URL}/${UTYPE_NEW}/${RANGE_DAY}/${date.getTime()}`;
+    const reqUrl = `${URL}/${UTYPE_NEW}/${RANGE_DAY}?date=${date.toISOString()}`;
     return this.http.get<number[]>(reqUrl);
   }
   getWeeklyNewUsersActivity(date: Date):  Observable<number[]> {
-    const reqUrl = `${URL}/${UTYPE_NEW}/${RANGE_WEEK}/${date.getTime()}`;
+    const reqUrl = `${URL}/${UTYPE_NEW}/${RANGE_WEEK}?date=${date.toISOString()}`;
     return this.http.get<number[]>(reqUrl);
   }
 
   getMonthlyNewUsersActivity(date: Date):  Observable<number[]> {
-    const reqUrl = `${URL}/${UTYPE_NEW}/${RANGE_MONTH}/${date.getTime()}`;
+    const reqUrl = `${URL}/${UTYPE_NEW}/${RANGE_MONTH}?date=${date.toISOString()}`;
     return this.http.get<number[]>(reqUrl);
   }
 
 
   getDailyReturningUsersActivity(date: Date):  Observable<number[]> {
-    const reqUrl = `${URL}/${UTYPE_RETURNING}/${RANGE_DAY}/${date.getTime()}`;
+    const reqUrl = `${URL}/${UTYPE_RETURNING}/${RANGE_DAY}?date=${date.toISOString()}`;
     return this.http.get<number[]>(reqUrl);
   }
 
   getWeeklyReturningUsersActivity(date: Date):  Observable<number[]> {
-    const reqUrl = `${URL}/${UTYPE_RETURNING}/${RANGE_WEEK}/${date.getTime()}`;
+    const reqUrl = `${URL}/${UTYPE_RETURNING}/${RANGE_WEEK}?date=${date.toISOString()}`;
     return this.http.get<number[]>(reqUrl);
   }
 
   getMonthlyReturningUsersActivity(date: Date):  Observable<number[]> {
-    const reqUrl = `${URL}/${UTYPE_RETURNING}/${RANGE_MONTH}/${date.getTime()}`;
+    const reqUrl = `${URL}/${UTYPE_RETURNING}/${RANGE_MONTH}?date=${date.toISOString()}`;
     return this.http.get<number[]>(reqUrl);
   }
 }
