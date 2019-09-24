7c6794f10319f83eff96582aca4f5a73f37de5bb
andziaania <andziaania@gmail.com>
2019-09-24 11:34:51 +0200

    perftrace.js - pages loading statistics

--- src/assets/perftrace.js
+++ src/assets/perftrace.js
@@ -1,97 +1,40 @@
-var OUR_STATISCICS_SERVER = 'http://localhost:8080/recorder';
-
-
-function informAboutAjaxRequest(ajaxRequestUrl) {
-    var req = new XMLHttpRequest();
-    var params = '?webappId=' + 1 + '&ajaxRequestUrl=' + ajaxRequestUrl
-    req.open("GET", OUR_STATISCICS_SERVER + "/registerAjaxRequest" + params, true);
-    req.setRequestHeader('Accept', 'application/json');
-    req.withCredentials = true; // pass along cookies
-
-    req.send();
-}
-
-
-//  Collecting page loading data.
- function collectPageLoadingStats(){
-        (async () => {
-          console.log('collectPageLoadingStats ' + (window.performance.timing.loadEventEnd) + "  " + window.performance.timing.navigationStart);
-
-          const rawResponse = await fetch(OUR_STATISCICS_SERVER  + "/registerPageLoading", {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                url: window.location.href,  //pathname. host
-                load: 123,
-//                load: window.performance.timing.loadEventEnd - window.performance.timing.navigationStart,
-//                timing: {
-//                    load: window.performance.timing.loadEventEnd - window.performance.timing.navigationStart,
-//                },
-            }),
-
-          });
-
-          const content = await rawResponse.json();
-
-          console.log(content);
-        })();
-}
-
-
 (function(window){
   console.log('perf-trace is perfect to trace!');
 
-  const PT_RECORDER_URL = 'api/recorder/initializeSession';
-
-  function post(params) {
+  function post(url, bodyParams, callback, callbackParams) {
+    PT_RECORDER_URL = '/api/recorder';
+    console.log('POSTING: ' + url);
+    const body = bodyParams ? JSON.stringify(bodyParams) : undefined;
     const req = new XMLHttpRequest();
-    req.open("POST", PT_RECORDER_URL, true);
-    // req.setRequestHeader("Accept", "application/json, text/plain, */*");
+
+    req.open("POST", this.PT_RECORDER_URL + url, true);
+    req.setRequestHeader("Content-Type", "application/json");
     req.withCredentials = true; // pass along cookies
-    req.send();
+    req.onload = function() {
+      if (callback) callback.apply(callbackParams);
+    }
+    req.send(body);
   }
 
-  function initSession() {
-    post('');
+  function postPageLoadingPerformance() {
+    const navigationPerformance = performance.getEntriesByType("navigation")[0];
+    const stats = {
+      "loadingTime": navigationPerformance.loadEventEnd - navigationPerformance.responseEnd,
+    }
+    post('/pageLoadingPerformance', stats);
+  }
+
+  function initSession(callback, callbackParams) {
+    post('/initializeSession', undefined, callback, callbackParams);
   }
 
 
+/*******************************************/
+  window.onload = function(){
+    setTimeout(function(){
+      // const pageLoadingEndTime = performance.now();
 
-initSession();
-
+      initSession(postPageLoadingPerformance);
+    }, 0);
+  }
 })(window)
-
-
-// Binding all server requests: Tracking user activities
-//(function(open) {
-//
-//    function isNotOurStatisticRequest(url) {
-//        return !url.includes(OUR_STATISCICS_SERVER);
-//    }
-//
-//    XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
-//
-//        this.addEventListener("readystatechange", function() {
-//            if (this.readyState == 4 && isNotOurStatisticRequest(this.responseURL)) {
-//                //informAboutAjaxRequest(this.responseURL);
-//                esAjaxReq(this.responseURL);
-//            }
-//            console.log(this.readyState); // this one I changed
-//        }, false);
-//
-//        open.apply(this, [].slice.call(arguments))
-//        // open.call(this, method, url, async, user, pass);
-//    };
-//
-//    var req = new XMLHttpRequest();
-//    var params = '?ajaxRequestUrl=' + "bzzzz";
-//    req.open("GET", OUR_STATISCICS_SERVER + "/initializeSession" + params, true);
-//    req.setRequestHeader('Accept', 'application/json');
-//    req.withCredentials = true; // pass along cookies
-//    req.send();
-//
-//})(XMLHttpRequest.prototype.open);
-
