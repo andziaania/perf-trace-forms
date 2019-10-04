298664fe5c679f0ac8646088ab08ee73cbf3882c
andziaania <andziaania@gmail.com>
2019-10-04 12:33:54 +0200

    Add multiple callbacks functionality for the recorder

--- src/assets/perftrace.js
+++ src/assets/perftrace.js
@@ -1,40 +1,52 @@
 (function(window){
-  console.log('perf-trace is perfect to trace!');
 
-  function post(url, bodyParams, callback, callbackParams) {
+  function post(url, bodyParams, callbacks) {
     PT_RECORDER_URL = '/api/recorder';
     console.log('POSTING: ' + url);
-    const body = bodyParams ? JSON.stringify(bodyParams) : undefined;
-    const req = new XMLHttpRequest();
+    const body = JSON.stringify(bodyParams);
 
+    const req = new XMLHttpRequest();
     req.open("POST", this.PT_RECORDER_URL + url, true);
     req.setRequestHeader("Content-Type", "application/json");
     req.withCredentials = true; // pass along cookies
-    req.onload = function() {
-      if (callback) callback.apply(callbackParams);
+    if (callbacks) {
+      req.onload = () => { callbacks.forEach(callback => callback()) };
     }
     req.send(body);
   }
 
-  function postPageLoadingPerformance() {
-    const navigationPerformance = performance.getEntriesByType("navigation")[0];
-    const stats = {
-      "loadingTime": navigationPerformance.loadEventEnd - navigationPerformance.responseEnd,
+  const recordingOnPageFunctions = {
+    postPageLoadingPerformance: function() {
+      const navigationPerformance = performance.getEntriesByType("navigation")[0];
+      const stats = {
+        loadingTime: navigationPerformance.loadEventEnd - navigationPerformance.responseEnd,
+      }
+      post('/pageLoadingPerformance', stats);
+    },
+
+    postPath: function() {
+      const param = {
+        path: window.location.pathname
+      }
+      post('/urlPath', param);
     }
-    post('/pageLoadingPerformance', stats);
   }
 
-  function initSession(callback, callbackParams) {
-    post('/initializeSession', undefined, callback, callbackParams);
-  }
+  const recordingOnPageCallbacks = Object.getOwnPropertyNames(recordingOnPageFunctions)
+          .filter(propName => typeof recordingOnPageFunctions[propName] === 'function')
+          .map(propName => recordingOnPageFunctions[propName]);
 
+
+  function initSession(callbacks) {
+    post('/initializeSession', undefined, callbacks);
+  }
 
 /*******************************************/
   window.onload = function(){
-    setTimeout(function(){
-      // const pageLoadingEndTime = performance.now();
+    console.log('perf-trace is perfect to trace!');
 
-      initSession(postPageLoadingPerformance);
+    setTimeout(function(){
+      initSession(recordingOnPageCallbacks);
     }, 0);
   }
 })(window)
