0eedcc2aff69d295fd70c242247fc9cb14b150fd
andziaania <andziaania@gmail.com>
2019-10-07 13:03:05 +0200

    ChartUrlsProportions: Dynamic color generation

--- src/app/pages/urls/chart-urls-proportions/chart-urls-proportions.component.ts
+++ src/app/pages/urls/chart-urls-proportions/chart-urls-proportions.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { BaseChartDirective } from 'ng2-charts';
+import { BaseChartDirective, Color, } from 'ng2-charts';
 
 @Component({
   selector: 'pt-chart-urls-proportions',
@@ -10,19 +10,29 @@ export class ChartUrlsProportionsComponent implements OnInit {
 
   @ViewChild(BaseChartDirective, { static: false }) chart: BaseChartDirective;
 
-  public pieChartLabels = ['UPSSS... No data ;P'];
-  public pieChartData = [1];
-  public pieChartType = 'pie';
+  pieChartLabels = ['UPSSS... No data ;P'];
+  pieChartData = [1];
+  pieChartType = 'pie';
+  pieChartColors =  [{ backgroundColor: ['rgba(110, 114, 20, .5)'] }];
 
   constructor() { }
 
   ngOnInit() {
+    this.updateColors();
   }
 
   public setChartData(data: number[], labels: string[]) {
     this.pieChartData = data;
     this.pieChartLabels = labels;
+    this.updateColors();
     this.chart.update();
   }
 
+  private updateColors() {
+    for (let id = this.pieChartColors[0].backgroundColor.length; id < this.pieChartData.length; id++) {
+      const genColor = () => Math.round(Math.random() * 255);
+      const newColor = `rgba(${genColor()}, ${genColor()}, ${genColor()}, 0.3)`;
+      this.pieChartColors[0].backgroundColor.push(newColor);
+    }
+  }
 }
